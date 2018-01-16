44ec7df50ede653c02472b555a76769466e4b9b7
Michael Freeman <michael.k.freeman@gmail.com>
2018-01-16 14:03:19 -0800

    Make ps-2, ex-1 test 9 more general

--- exercise-1/exercise-1.spec.js
+++ exercise-1/exercise-1.spec.js
@@ -16,104 +16,104 @@ let cssObj = css.parse(cssFile, options);
 
 
 describe('Testing CSS', () => {
-	let $; //cheerio instance
-	
-	// Inline the stylesheet
-    beforeAll( () => {
+    let $; //cheerio instance
+
+    // Inline the stylesheet
+    beforeAll(() => {
         let options = {
-        	extraCss:cssFile, 
-        	url:'file://'+__dirname + '/'
+            extraCss: cssFile,
+            url: 'file://' + __dirname + '/'
         }
         return inlineCss(html, options)
-        		.then(function(htmlStyled) { 
-    	    	$ = cheerio.load(htmlStyled);        	
-        	}).catch(function(err) {
-	        	console.log(err)
+            .then(function(htmlStyled) {
+                $ = cheerio.load(htmlStyled);
+            }).catch(function(err) {
+            console.log(err)
         });
     })
-	test('1. Box-sizing is set to border-box', () => {
-		let body = $('body');
-		expect(body.css('box-sizing')).toEqual('border-box');
-		
-	})
-	
-	test('2. Spacing set around main element', () => {
-		let main = $('main');
-		expect(main.css('width')).toEqual('80%');
-		expect(main.css('margin-left')).toEqual('auto');		
-		expect(main.css('margin-right')).toEqual('auto');		
-		expect(main.css('margin-top')).toEqual('5em');				
-		
-	})
-	
-	test('3. Set appropriate colors, fonts, and sizes:', () => {
-		let body = $('body');
-		let h1 = $("h1")
-		expect(body.css('color')).toEqual('#535353');
-		expect(h1.css('color')).toEqual('#4b2e83');
-		expect(h1.css('font-size')).toEqual('2.5rem');		
-		expect(body.css('font-family')).toMatch('Open Sans');	
-		
-	})	
-	
-	test("4. Style nav bar", () => {
-		let nav = $('nav')
-		expect(nav.css('position')).toEqual('fixed')
-		expect(nav.css('background-color')).toEqual('#4b2e83')
-		expect(nav.css('padding')).toEqual('.75rem')		
-	})
-	
-	test("5. Style nav bar links", () => {
-		let links = $('nav a')
-		expect(links.css('color')).toEqual('white')
-		expect(links.css('text-decoration')).toEqual('none')
-	})
-	
-	test("6. Display list items", () => {
-		let lis = $('nav li')
-		expect(lis.css('display')).toEqual('inline')
-		expect(lis.css('padding')).toEqual('1rem')
-		let ul = $('nav ul')
-		expect(ul.css('margin')).toEqual("0px")
-		expect(ul.css('padding-top')).toEqual('.5rem')		
-	})
-	
-	test("7. Display search box", () => {
-		let search = $('div.searchBox')
-		expect(search.css('float')).toEqual('right')
-		let searchInput = $('div.searchBox input')
-		expect(searchInput.css('font-size')).toEqual('1rem')
-		expect(searchInput.css('padding')).toEqual('.5rem')		
-	})	
-	
-	test("8. Font awesome icon styles", () => {
-		let fa = $('.fa')
-		expect(fa.css('font-size')).toEqual("1.5rem")
-		expect(fa.css('padding')).toEqual(".3rem")	
-		expect(fa.attr('aria-label')).toMatch("search")
-	})		
-	
-	test("9. Test hover effects", () => {
- 		let hoverRule = cssObj.stylesheet.rules.filter((d) => d.type === "rule" && d.selectors.indexOf("a:hover") !== -1);
- 		let focusRule = cssObj.stylesheet.rules.filter((d) => d.type === "rule" && d.selectors.indexOf("a:focus") !== -1);
- 		let colorRule = hoverRule[0].declarations.filter((d) => d.property === "color")[0]
- 		let borderRule = hoverRule[0].declarations.filter((d) => d.property === "border-bottom")[0]
- 		expect(hoverRule.length).toBeGreaterThan(0)
- 		expect(focusRule.length).toBeGreaterThan(0) 		
-		expect(colorRule.value).toEqual('#b7a57a')
-		expect(borderRule.value).toEqual('.6rem solid #b7a57a')
-	})		
-	
-	test("10. Add and style logo", () => {
-		let img = $('.logo')
-		expect(img.css('background-image')).toEqual("url('../img/ischool-symbol-white.png')")
-		expect(img.css('padding')).toEqual('0 1em')
-		expect(img.css('background-size')).toEqual('contain')
-		expect(img.css('background-repeat')).toEqual('no-repeat')
-		expect(img.css('background-position')).toEqual('left')
-		
-	})
-	
+    test('1. Box-sizing is set to border-box', () => {
+        let body = $('body');
+        expect(body.css('box-sizing')).toEqual('border-box');
+
+    })
+
+    test('2. Spacing set around main element', () => {
+        let main = $('main');
+        expect(main.css('width')).toEqual('80%');
+        expect(main.css('margin-left')).toEqual('auto');
+        expect(main.css('margin-right')).toEqual('auto');
+        expect(main.css('margin-top')).toEqual('5em');
+
+    })
+
+    test('3. Set appropriate colors, fonts, and sizes:', () => {
+        let body = $('body');
+        let h1 = $("h1")
+        expect(body.css('color')).toEqual('#535353');
+        expect(h1.css('color')).toEqual('#4b2e83');
+        expect(h1.css('font-size')).toEqual('2.5rem');
+        expect(body.css('font-family')).toMatch('Open Sans');
+
+    })
+
+    test("4. Style nav bar", () => {
+        let nav = $('nav')
+        expect(nav.css('position')).toEqual('fixed')
+        expect(nav.css('background-color')).toEqual('#4b2e83')
+        expect(nav.css('padding')).toEqual('.75rem')
+    })
+
+    test("5. Style nav bar links", () => {
+        let links = $('nav a')
+        expect(links.css('color')).toEqual('white')
+        expect(links.css('text-decoration')).toEqual('none')
+    })
+
+    test("6. Display list items", () => {
+        let lis = $('nav li')
+        expect(lis.css('display')).toEqual('inline')
+        expect(lis.css('padding')).toEqual('1rem')
+        let ul = $('nav ul')
+        expect(ul.css('margin')).toEqual("0px")
+        expect(ul.css('padding-top')).toEqual('.5rem')
+    })
+
+    test("7. Display search box", () => {
+        let search = $('div.searchBox')
+        expect(search.css('float')).toEqual('right')
+        let searchInput = $('div.searchBox input')
+        expect(searchInput.css('font-size')).toEqual('1rem')
+        expect(searchInput.css('padding')).toEqual('.5rem')
+    })
+
+    test("8. Font awesome icon styles", () => {
+        let fa = $('.fa')
+        expect(fa.css('font-size')).toEqual("1.5rem")
+        expect(fa.css('padding')).toEqual(".3rem")
+        expect(fa.attr('aria-label')).toMatch("search")
+    })
+
+    test("9. Test hover effects", () => {
+        let hoverRule = cssObj.stylesheet.rules.filter((d) => d.type === "rule" && d.selectors.join().indexOf("a:hover") !== -1);
+        let focusRule = cssObj.stylesheet.rules.filter((d) => d.type === "rule" && d.selectors.join().indexOf("a:focus") !== -1);
+        let colorRule = hoverRule[0].declarations.filter((d) => d.property === "color")[0]
+        let borderRule = hoverRule[0].declarations.filter((d) => d.property === "border-bottom")[0]
+        expect(hoverRule.length).toBeGreaterThan(0)
+        expect(focusRule.length).toBeGreaterThan(0)
+        expect(colorRule.value).toEqual('#b7a57a')
+        expect(borderRule.value).toEqual('.6rem solid #b7a57a')
+    })
+
+    test("10. Add and style logo", () => {
+        let img = $('.logo')
+        expect(img.css('background-image')).toEqual("url('../img/ischool-symbol-white.png')")
+        expect(img.css('padding')).toEqual('0 1em')
+        expect(img.css('background-size')).toEqual('contain')
+        expect(img.css('background-repeat')).toEqual('no-repeat')
+        expect(img.css('background-position')).toEqual('left')
+
+    })
+
 })
 describe('Source code is valid', () => {
     test('HTML validates without errors', async () => {
